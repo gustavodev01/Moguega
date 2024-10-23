43fcdc92c876bda846be87339819f35c5f902161
Gustavo Santana <gustavosantana1502@gmail.com>
2024-10-23 19:37:29 -0300

    restruturação do projeto e adição de novos elementos html, css e javascript

---
+++ javascript/index/index.js
@@ -0,0 +1,10 @@
+function incluirHeader() {
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o header:', error));
+}
+
+incluirHeader();
