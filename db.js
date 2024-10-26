37e04644901d7c6a2b5060279fd0ed2d1c18fc1b
Gustavo Santana <gustavosantana1502@gmail.com>
2024-10-26 15:44:29 -0300

    Criado variaveis de ambiente para armanezar conexão com DB e criado arquivo gitignore

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'BarbeariaApp',
-  password: '!@#admin',
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Exportar a pool para ser usada em outros lugares
