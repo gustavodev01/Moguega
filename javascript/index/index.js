8972c65b021247425f4252d5e90e338124324d82
Gustavo Santana <gustavosantana1502@gmail.com>
2024-11-09 13:22:11 -0300

    adicionado mais alguns ajustes de clique na tela principal

--- javascript/index/index.js
+++ javascript/index/index.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   incluirHeader();
 
-  const abrirModal = document.getElementById("abrirModal");
+  const inputLocalizacao = document.getElementById("abrirModal");
+  const botaoVerBarbearias = document.getElementById("botaoVerBarbearias");
+  const modal = new bootstrap.Modal(document.getElementById('locationModal'));
 
   // Verifica se o elemento existe antes de adicionar o evento
-  if (abrirModal) {
-      abrirModal.addEventListener("click", function () {
-          const modal = new bootstrap.Modal(document.getElementById('locationModal'));
+  if (inputLocalizacao) {
+      inputLocalizacao.addEventListener("click", function () {
           modal.show();
       });
   } else {
       console.error("Elemento com id 'abrirModal' não encontrado.");
+  }
+
+  // Adiciona evento de clique ao botão para abrir o modal
+  if (botaoVerBarbearias) {
+      botaoVerBarbearias.addEventListener("click", function () {
+          modal.show();
+      });
+  } else {
+      console.error("Elemento com id 'botaoVerBarbearias' não encontrado.");
   }
 });
 
